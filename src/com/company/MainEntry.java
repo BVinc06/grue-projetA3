ca643bb3fff0a375d51ab8fe60d605b7fb3c94c9
Alexisssssssss <alexis.georges@viacesi.fr>
2019-09-24 10:29:47 +0200

    First Commit - Structure

---
+++ src/com/company/MainEntry.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class MainEntry {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/company/model/CLmapTB_PERSONNE.java
@@ -0,0 +1,4 @@
+package com.company.model;
+
+public class CLmapTB_PERSONNE {
+}
